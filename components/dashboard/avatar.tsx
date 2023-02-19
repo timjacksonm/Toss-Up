ab1f80fc2908929408a0dc57f83308f2d3e13d11
Tim Jackson <timjacksonm@gmail.com>
2023-02-19 00:30:46 -0600

    Added avatar component

---
+++ components/dashboard/avatar.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { User } from '@prisma/client';
+import * as Avatar from '@radix-ui/react-avatar';
+import { useEffect, useState } from 'react';
+
+export function UserAvatar({
+  user: { image, firstName, lastName },
+}: {
+  user: User;
+}) {
+  const [initials, setInitials] = useState<string>();
+
+  useEffect(() => {
+    if (firstName && lastName) {
+      const uppercase = (
+        firstName.charAt(0) + lastName.charAt(0)
+      ).toUpperCase();
+      setInitials(uppercase);
+    }
+  }, [firstName, lastName]);
+  return (
+    <Avatar.Root className="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <Avatar.Image
+        className="h-full w-full rounded-[inherit] object-cover"
+        src={image ?? undefined}
+      />
+      <Avatar.Fallback className="leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium">
+        {initials}
+      </Avatar.Fallback>
+    </Avatar.Root>
+  );
+}
