3ad3da32d0ab9aba93074fc344c854dcb9e9b1e0
Tim Jackson <timjacksonm@gmail.com>
2023-03-25 00:00:06 -0500

    rm build step in CI since vercel does that. added ignoreDuringBuilds for eslint and ignoreBuildErrors for typescript when vercel does build

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@
 const nextConfig = {
   typescript: {
     tsconfigPath: './tsconfig.json',
+    ignoreBuildErrors: true,
   },
   eslint: {
     dirs: ['pages', 'components', 'app', 'styles', 'lib'],
+    ignoreDuringBuilds: true,
   },
   experimental: {
     appDir: true,
   },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
